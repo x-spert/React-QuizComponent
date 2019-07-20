2b34aadbc5e25cf8b7ed43df062d2a16934f8058
Razvan Gantu <razvangantu@gmail.com>
2019-07-20 16:41:14 +0300

    module2

---
+++ src/QuizQuestion.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section>
+                    <ul>
+                        <li>{this.props.quiz_question.answer_options[0]}</li>
+                    </ul>
+                </section>
+            </main>
+        );
+    }
+}
+
+export default QuizQuestion;
